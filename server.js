fad828d8efd9ecdedcd1597d56b06a0c575f150e
Austin Jess <ajess33@gmail.com>
2019-01-22 16:12:50 -0600

    now works with unix input

--- server.js
+++ server.js
@@ -9,33 +9,33 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-const dateOptions = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-};
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-// if there isnt a date, return the current time
-app.get('/api/timestamp', (req, res) => {
-  const dateValue = moment().format('ddd Do MMM YYYY');
-  const unixDate = moment().unix();
-
-  res.json({ unix: unixDate, utc: dateValue });
 });
 
 app.get('/api/timestamp/:dateValue', (req, res) => {
   let { dateValue } = req.params;
 
   if (moment(dateValue, 'YYYY-MM-DD').isValid()) {
-    const unixDate = moment(dateValue, 'YYYY-MM-DD').unix();
+    const unixDate = moment(dateValue, 'YYYY-MM-DD').unix() * 1000;
     dateValue = moment(dateValue, 'YYYY-MM-DD').format('ddd Do MMM YYYY');
     res.json({ unix: unixDate, utc: dateValue });
+  } else {
+    if (moment(dateValue, 'X').isValid()) {
+      const unixDate = moment(dateValue, 'X').unix() * 1000;
+      dateValue = moment(unixDate, 'x').format('ddd Do MMM YYYY');
+      res.json({ unix: unixDate, utc: dateValue });
+    }
   }
   res.json({ error: 'Invalid Date' });
+});
+
+// if there isnt a date, return the current time
+app.get('/api/timestamp', (req, res) => {
+  const dateValue = moment().format('ddd Do MMM YYYY');
+  const unixDate = moment().unix() * 1000;
+
+  res.json({ unix: unixDate, utc: dateValue });
 });
 
 app.listen(3000, () => {
